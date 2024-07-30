e1ede8522ee96964232228d7c96622a571702dfa
NathanDahlquist <nathan.j.dahlquist@gmail.com>
2024-07-30 10:41:24 -0400

    Created and populated the models for later use.

--- Final_Project/Models/FavoriteBreakfast.cs
+++ Final_Project/Models/FavoriteBreakfast.cs
@@ -2,5 +2,10 @@
 {
     public class FavoriteBreakfast
     {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string BreakfastName { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
     }
 }
