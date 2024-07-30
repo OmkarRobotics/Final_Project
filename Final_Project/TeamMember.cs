5be3d4f09fee9dc7c8f5f958dfa8fd013d6fc4f7
Sunav Adhikari <adhikas2@mail.uc.edu>
2024-07-30 09:53:01 -0400

    teammember class

---
+++ Final_Project/TeamMember.cs
@@ -0,0 +1,17 @@
+namespace Final_Project
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+
+        public int[] Birthdate { get; set; }
+
+        public string CollegeProgram { get; set; }
+
+        public string Year { get; set; }
+
+        public string Hobby { get; set; }
+
+
+    }
+}
