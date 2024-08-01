24ef9f605f74decf1fa608e84e86682ae22fb95b
OmkarRobotics <122411633+OmkarRobotics@users.noreply.github.com>
2024-08-01 10:57:23 -0400

    Added Startup.cs file

---
+++ Final_Project/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Final_Project.Data;
+using Final_Project.Interfaces;
+using Microsoft.AspNetCore.Builder; 
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting; 
+namespace Final_Project
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;  
+        
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        { 
+            services.AddControllers();
+            services.AddSwaggerDocument();
+            services.AddDbContext<TeamMemberContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TeamMemberContext")));
+            services.AddScoped<ITeamMemberContextDAO, TeamMemberContextDAO>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, TeamMemberContext context) 
+        {
+            if (env.IsDevelopment())
+            { 
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseOpenAPI();
+            app.UseSwaggerUI3(); 
+            context.Database.Migrate();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
