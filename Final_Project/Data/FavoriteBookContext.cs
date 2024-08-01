37b46ba086d9c7c6ff96e3befa719c14d6e37e6c
OmkarRobotics <122411633+OmkarRobotics@users.noreply.github.com>
2024-08-01 09:37:38 -0400

    Added information to favorite book context class

--- Final_Project/Data/FavoriteBookContext.cs
+++ Final_Project/Data/FavoriteBookContext.cs
@@ -12,10 +12,10 @@ namespace Final_Project.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<FavoriteBook>().HasData(
-                new FavoriteBook(1, "Omkar Seth", "11/12", "College Credit Plus", "Freshman"),
-                new FavoriteBook(2, "Sunav Adhikari", "01/8", "College Credit Plus", "Freshman"),
-                new FavoriteBook(3, "Nathan Dahlquist", "07/29", "Information Technology", "Junior"),
-                new FavoriteBook(4, "Sydney Hutton", "02/18", "Information Technology", "Junior")
+                new FavoriteBook(1, "Omkar Seth", "Animal Farm", 5.99, "08/17/1945"),
+                new FavoriteBook(2, "Sunav Adhikari", "Harry Potter and the Goblet of Fire", 9.95, "07/08/2000"),
+                new FavoriteBook(3, "Nathan Dahlquist", "House of Leaves", 14.95, "03/07/2000"),
+                new FavoriteBook(4, "Sydney Hutton", "Ms.Peregrines Home for Peculiar Children", 12.99, "06/07/2011")
                 );
         }
 
