f050d8d6b2a5401c2b941eebd27067042b440b3d
huttonsy <huttonsy@mail.uc.edu>
2024-08-02 09:26:03 -0400

    controllers

---
+++ Final_Project/Controllers/FavoriteBreakfastController.cs
@@ -0,0 +1,43 @@
+using Final_Project.Data;
+using Final_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FavoriteBreakfastController : ControllerBase
+    {
+        private readonly ILogger<FavoriteBreakfastController> _logger;
+        private readonly ApplicationDBContext _context;
+
+        public FavoriteBreakfastController(ILogger<FavoriteBreakfastController> logger, ApplicationDBContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(int id, string name, string description, double price)
+        {
+            if (_context.AddFavoriteBreakfast(id, name, description, price) > 0)
+            {
+                return Ok("Favorite breakfast added.");
+            }
+            else
+            {
+                return StatusCode(500, "An internal error occurred.");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Read(int? id)
+        {
+            if (_context.GetFavoriteBreakfast(id).Count == 1)
+            {
+                return Ok(_context.GetFavoriteBreakfast(id)[0]);
+            }
+            return Ok(_context.GetFavoriteBreakfast(id));
+        }
+    }
+}
